e1913a8c597be7ee559f39a8faa3517420421cd6
Harsh Pandey <pandeyharsh704@gmail.com>
2023-05-09 17:37:33 +0530

    This is the first commit

---
+++ src/components/Background.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Background = () => {
+  return (
+    <div>
+      <ul className="background">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Background;
